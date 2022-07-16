c05e7c0708c26c3008088a42914d2cdf9ee71df7
Valeriia <volikavs@gmail.com>
2022-07-16 10:55:23 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-import './js/headerrefs.js';
-// =====  header
-import './js/header';
+// Styles
+import './sass/main.scss';
+
+// Scripts
+import './js/search';
+import './js/render-movie-cards';
+import './js/movie-modal';
+import './js/pagination';
+import './js/team-modal';
+import './js/watched';
+import './js/queue';
+import './js/scroll-up';
