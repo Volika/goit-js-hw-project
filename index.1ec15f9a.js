73866540caad489d3eae55d61e865a1247b7274c
Volika <volikavs@gmail.com>
2022-07-16 08:56:01 +0000

    Deploying to gh-pages from @ Volika/goit-js-hw-project@c05e7c0708c26c3008088a42914d2cdf9ee71df7 🚀

--- index.1ec15f9a.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".header"),document.querySelector("header__logo");
-//# sourceMappingURL=index.1ec15f9a.js.map
--- index.d9e15c56.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".header"),document.querySelector("header__logo");
-//# sourceMappingURL=index.d9e15c56.js.map
